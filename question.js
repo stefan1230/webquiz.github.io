74ce430ea5fceaf0c4c51aa9410b20b49ade3a51
stefan1230 <37437060+stefan1230@users.noreply.github.com>
2018-03-18 14:40:42 +0530

    first project

---
+++ question.js
@@ -0,0 +1,117 @@
+//1
+var questions = [{
+	"question" : "Inside which HTML element do we put the JavaScript?",
+	"option1"  : "<js> ",
+	"option2"  : "<javascript>  ",
+	"option3"  : "<scripting> ",
+	"option4"  : "<script>  ",
+	"answer"   : "4"
+},{//2
+    "question" : "Where is the correct place to insert a JavaScript?",                  
+	"option1"  : " Both the head section and the body section are correct ",
+	"option2"  : "The head section   ",
+	"option3"  : "The body section  ",
+	"option4"  : "none of these ",
+	"answer"   : "1"
+    
+},
+   {//3
+    "question" : "What is the correct syntax for referring to an external script called 'xxx.js'?",                
+	"option1"  : "<script name='xxx.js'>",
+	"option2"  : "<script href='xxx.js'>",
+	"option3"  : "<script src='xxx.js'>",
+	"option4"  : "none of these",
+	"answer"   : "3"
+    
+},
+                 
+{//4
+    "question" : "What is the correct JavaScript syntax to change the content of the HTML element below?         <p id='demo'>This is Documentation</p>" ,               
+	"option1"  : "<script name='xxx.js'>",
+	"option2"  : "<script href='xxx.js'>",
+	"option3"  : "<script src='xxx.js'>",
+	"option4"  : "none of these",
+	"answer"   : "3"
+    
+},
+    {//5
+    "question" : "The external JavaScript file must contain the <script> tag.",                
+	"option1"  : "True ",
+	"option2"  : "False",
+	"option3"  : "Depends on the type of include ",
+	"option4"  : "Depends on the type of include ",
+	"answer"   : "2"
+    
+},
+ {//6
+    "question" : "How do you write 'Hello World' in an alert box?",                
+	"option1"  : "msgBox('Hello World'); ",
+	"option2"  : " alert('Hello World'); ",
+	"option3"  : "alertBox('Hello World'); ",
+	"option4"  : "msg('Hello World');  ",
+	"answer"   : "2"
+    
+},
+                 
+{//7
+    "question" : "How do you create a function in JavaScript?",                
+	"option1"  : "function = myFunction()  ",
+	"option2"  : "  function:myFunction()  ",
+	"option3"  : "function myFunction()  ",
+	"option4"  : "function::myFunction()   ",
+	"answer"   : "3"
+    
+},
+                 {//8
+    "question" : "How do you call a function named 'myFunction'?",                
+	"option1"  : "call function myFunction()  ",
+	"option2"  : "  myFunction()   ",
+	"option3"  : "call myFunction()  ",
+	"option4"  : "go myFunction()    ",
+	"answer"   : "2"
+    
+},
+                               {//9
+    "question" : "How to write an IF statement in JavaScript?",                
+	"option1"  : "if (i == 5)   ",
+	"option2"  : " if i = 5 then   ",
+	"option3"  : "if i == 5 then   ",
+	"option4"  : "if i = 5     ",
+	"answer"   : "1"
+    
+},
+                 
+                                         {//10
+    "question" : "How does a WHILE loop start?",                
+	"option1"  : "while (i <= 10; i++)   ",
+	"option2"  : " while (i <= 10)  ",
+	"option3"  : "while i = 1 to 10   ",
+	"option4"  : "none of these     ",
+	"answer"   : "2"
+    
+}
+                 
+                 
+                 
+                 
+              
+                 
+                
+                
+                
+                
+                
+                
+                
+                
+            
+                
+                
+                
+                
+                
+                
+                
+                
+                
+];
